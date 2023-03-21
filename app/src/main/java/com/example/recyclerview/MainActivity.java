a99a396a35f3fe679b91b703e1ab702214e95eb8
Rakib Hossen <rakibhossen1660@gmail.com>
2023-03-21 08:00:57 +0600

    set adapter and run app

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> cityNames= new ArrayList<>(Arrays.asList("Dhaka","Mymensingh","Seedstore","Bhaluka","Mawna","MasterBari"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        MyAdapter adapter = new MyAdapter(this,cityNames);
+        recyclerView.setAdapter(adapter);
 
 
     }
