694a0100f34de6b9e984ebdb20effe4ebe1cd603
Rakib Hossen <rakibhossen1660@gmail.com>
2023-03-21 07:43:22 +0600

    create adapter

---
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -0,0 +1,40 @@
+package com.example.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    ArrayList<String> cityNames;
+    Context context;
+
+    public MyAdapter(Context context,ArrayList<String> cityNames) {
+        this.context = context;
+        this.cityNames = cityNames;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.card_view,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.nameText.setText(cityNames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cityNames.size();
+    }
+}
