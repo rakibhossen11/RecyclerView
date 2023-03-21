1429efdcf192f1bb1c84bc800884fa9da168cb30
Rakib Hossen <rakibhossen1660@gmail.com>
2023-03-21 08:07:49 +0600

    add onclick listener

--- app/src/main/java/com/example/recyclerview/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,6 +32,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.nameText.setText(cityNames.get(position));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, cityNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
